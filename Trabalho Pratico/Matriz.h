84f533012beadbe9048972dc896afab919d33ebf
André Silva <andres.silva010@gmail.com>
2012-10-19 00:24:34 +0100

    Adição da Matriz Dinâmica em Matriz.h. Alterações a main.cpp para funcionar com Transporte.h. Remoção dos atributos key1 e key2 em Transporte.h.

---
+++ Trabalho Pratico/Matriz.h
@@ -0,0 +1,123 @@
+#ifndef Matriz_
+#define Matriz_
+
+#include "Transporte.h"
+
+class Matriz{
+private:
+	Transporte* *mat;
+
+	int* keys;
+
+	int key_atual;
+	int dim;
+
+	void aumenta();
+	int encontra(int key);
+public:
+	Matriz(int d);
+	Matriz(const Matriz& m);
+	~Matriz();
+
+	void addValor(int key1, int key2, float temp, float dist);
+	void retValor(Transporte &x);
+};
+
+Matriz::Matriz(int d){
+	key_atual=0;
+	dim=d;
+	
+	mat=new Transporte*[dim];
+	for(int i=0;i<dim;i++)
+		mat[i]=new Transporte[dim];
+
+	keys=new int[dim];
+}
+
+Matriz::Matriz(const Matriz &m){
+	key_atual=m.key_atual;
+	dim=m.dim;
+		
+	mat=new Transporte*[dim];
+	for(int i=0;i<key_atual;i++)
+		mat[i]=new Transporte[dim];
+	
+	for(int i=0;i<key_atual;i++)
+		for(int j=0;j<key_atual;j++)
+			mat[i][j]=m.mat[i][j]->clone();
+	
+	keys=new int[dim];
+	for(int i=0;i<key_atual;i++)
+		keys[i]=m.keys[i]->clone();
+}
+
+Matriz::~Matriz(){
+	for(int i=0;i<key_atual;i++){
+		for(int j=0;j<key_atual;j++)
+			delete mat[i][j];
+		delete mat[i];
+	}
+	delete[]mat;
+
+	for(int i=0;i<key_atual;i++)
+		delete keys[i];
+	delete [] keys;
+	
+}
+
+void Matriz::aumenta(){
+	Transporte ** tmp;
+	tmp = new Transporte *[2*dim];
+	for(int i=0;i<key_atual;i++)
+		tmp[i]=new Transporte[2*dim];
+	
+	for(int i=0;i<key_atual;i++)
+		for(int j=0;j<key_atual;j++)
+			tmp[i][j]=mat[i][j];
+	delete[]mat;
+	mat=tmp;
+
+	int* tmpk;
+	tmpk=new int *[2*dim];
+	for(int i=0;i<key_atual;i++)
+		tmpk[i]=keys[i];
+	delete[]keys;
+	keys=tmpk;
+
+	dim*=2;
+}
+
+int Matriz::encontra(int key){
+	int posk=0;
+	for(;posk<key_atual;posk1++)
+		if(*keys[i]==key)
+			break;
+	if(posk==key_atual)
+		if(key_atual!=dim){
+			*keys[posk]=key1;
+			key_atual++;
+		}else{
+			aumenta();
+			*keys[posk]=key1;
+			key_atual++;
+		}
+
+	return posk;
+}
+
+void Matriz::addValor(int key1, int key2, float temp, float dist){
+	int posk1=encontra(key1);
+	int posk2=encontra(key2);
+	
+	Transporte tmp(dist, temp);
+	mat[posk1][posk2]=&tmp;
+}
+
+void Matriz::retValor(Transporte &x, int key1, int key2){
+	int posk1=encontra(key1);
+	int posk2=encontra(key2);
+
+	x=mat[posk1][posk2];
+}
+
+#endif
--- Trabalho Pratico/Transporte.h
+++ Trabalho Pratico/Transporte.h
@@ -3,26 +3,20 @@
 
 class Transporte{
 private:
-	int key1;
-	int key2;
 	float distanciametros;
 	float tempominutos;
 
 public:
 	Transporte();
-	Transporte(int k1, int k2, float distm, float tmpm);	
+	Transporte(float distm, float tmpm);	
 	~Transporte();
 
 	void setDistanciametros(float distm);
 	void setTempominutos(float tmpmin);
-	void setKey1(int k1);
-	void setKey2(int k2);
-
+	
 	float getDistanciametros() const;
 	float getTempominutos() const;
-	int getKey1() const;
-	int getKey2() const;
-
+	
 	bool operator==(const Transporte &t);
 	bool operator>(const Transporte &t);
 	Transporte& operator=(const Transporte &t);
@@ -30,15 +24,11 @@ public:
 };
 
 Transporte::Transporte(){
-	setKey1(0);
-	setKey2(0);
 	setDistanciametros(0);
 	setTempominutos(0);
 }
 
-Transporte::Transporte(int k1, int k2, float distm, float tmpm){
-	setKey1(k1);
-	setKey2(k2);
+Transporte::Transporte(float distm, float tmpm){
 	setDistanciametros(distm);
 	setTempominutos(tmpm);
 }
@@ -54,14 +44,6 @@ void Transporte::setTempominutos(float tmpmin){
 	tempominutos=tmpmin;
 }
 
-void Transporte::setKey1(int k1){
-	key1=k1;
-}
-
-void Transporte::setKey2(int k2){
-	key2=k2;
-}
-
 float Transporte::getDistanciametros() const{
 	return distanciametros;
 }
@@ -70,16 +52,8 @@ float Transporte::getTempominutos() const{
 	return tempominutos;
 }
 
-int Transporte::getKey1() const{
-	return key1;
-}
-
-int Transporte::getKey2() const{
-	return key2;
-}
-
 bool Transporte::operator==(const Transporte &t){
-	return (key1==t.key1 && key2==t.key2 && distanciametros==t.distanciametros && tempominutos==t.tempominutos);
+	return (distanciametros==t.distanciametros && tempominutos==t.tempominutos);
 }
 	
 bool Transporte::operator>(const Transporte &t){
@@ -88,8 +62,6 @@ bool Transporte::operator>(const Transporte &t){
 
 Transporte& Transporte::operator=(const Transporte &t){
 	if(this!=&t){
-		key1=t.key1;
-		key2=t.key2;
 		distanciametros=t.distanciametros;
 		tempominutos=t.tempominutos;
 	}
@@ -98,7 +70,7 @@ Transporte& Transporte::operator=(const Transporte &t){
 }
 
 void Transporte::escreve(ostream& out) const{
-	out << "Viagem entre " << key1 << " e " << key2 << " e de " << tempominutos << " minutos, com uma distancia de " << distanciametros << " metros." << endl;
+	out << "Viagem e de " << tempominutos << " minutos, com uma distancia de " << distanciametros << " metros." << endl;
 }
 
 ostream & operator << (ostream &out, Transporte &t){
