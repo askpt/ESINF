7c140318c280198007fe8c4cca6c032481fa4823
JoniDragons2 <joni__joao@hotmail.com>
2012-10-18 22:30:46 +0100

    Tratamento de leitura de ficheiros.

--- Trabalho Pratico/main.cpp
+++ Trabalho Pratico/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <string>
-
+#include <fstream>
 using namespace std;
 
 #include "Robot.h"
@@ -15,7 +15,61 @@ void main(){
 	Armazem ar1(1231, 122, 123);
 	Robot r1 (225, 25878, 4858, &ar1);
 	Transporte tr(at1.getKey(), ar1.getKey(), 12312,1222);
+	//Ficheiros
 
+	int i = 0;
+	int s=0;
+	string linha;
+
+	ifstream fx;
+	fx.open("C:\\Users\\Joni\\Desktop\\Robot.txt");
+	if(!fx)
+		cout << "Ficheiro não existe! " << endl;
+	else{
+		string *temp = new string [1000];
+		while(!fx.eof()){
+			getline(fx, linha, '\n');
+			if(linha.size() > 0) {
+				int inicio = 0;
+				int pos = linha.find(',', inicio);
+				if(s == 0){
+					/* Simplesmente para retirar o primeiro atributo
+					do ficheiro de robots. Simplesmente troca-se esta validação pela
+					validação pelo nome do ficheiro e está feito.*/
+					temp[0] = (linha.substr(inicio, pos-inicio));
+					s++;
+					i++;
+				}else{
+					/*Lista Ligada */temp[i] = (linha.substr(inicio, pos-inicio)); 
+					i++;
+					pos++;
+					inicio = pos;
+					pos = linha.find(',', inicio);
+					temp[i] = (linha.substr(inicio,pos-inicio));
+					i++;
+					pos++;
+					inicio = pos;
+					pos = linha.find(',',inicio);
+					temp[i] = (linha.substr(inicio, pos-inicio));
+					i++;
+					pos++;
+					inicio=pos;
+					pos = linha.find(',',inicio);
+					temp[i] = (linha.substr(inicio,pos-inicio));
+					i++;
+					pos++;
+				}
+			}
+		}
+		fx.close();
+		for(int j = 0; j < i; j++){
+			cout << temp[j] << endl;
+		}
+	}
+
+	cin.get();
+
+	//
 	cout << at1;
 	cout << ar1;
 	cout << r1;
