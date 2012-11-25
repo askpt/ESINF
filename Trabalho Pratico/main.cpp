9f5e5433690563ea63eac394ab3a8e00b5bd70fc
JoniDragons2 <joni__joao@hotmail.com>
2012-11-25 14:39:48 +0000

    Issue #9
    Update ao menu. Introduzidas algumas validações e nova opção: 12 - Reset
    das posições dos robots.

--- Trabalho Pratico/main.cpp
+++ Trabalho Pratico/main.cpp
@@ -24,19 +24,23 @@ void menu(){
 
 	string nome;
 	int esc=0,opcao=0;
+	int cont = 0;
 	do{
 		cout << "1 - Leitura De Ficheiro" << endl;
-		cout << "2 - Apresentar Matriz" << endl;
-		cout << "3 - Apresentar Lista" << endl;
-		cout << "4 - Apresentar Queue" << endl;
-		cout << "5 - Apresentar Grafo" << endl;
-		cout << "6 - Insere no Grafo" << endl;
-		cout << "7 - Valida o Grafo" << endl;
-		cout << "8 - Abastecer um posto automatico" << endl;
-		cout << "9 - Abastecer um posto armazem" << endl;
-		cout << "10 - Abastecer Automaticamente" << endl;
-		cout << "11 - Mostra estado fabrica" << endl;
-		cout << "0 - Sair" << endl;
+		if(fab.getCont() == 4){
+			cout << "2 - Apresentar Matriz" << endl;
+			cout << "3 - Apresentar Lista" << endl;
+			cout << "4 - Apresentar Queue" << endl;
+			cout << "5 - Apresentar Grafo" << endl;
+			cout << "6 - Insere no Grafo" << endl;
+			cout << "7 - Valida o Grafo" << endl;
+			cout << "8 - Abastecer um posto automatico" << endl;
+			cout << "9 - Abastecer um posto armazem" << endl;
+			cout << "10 - Abastecer Automaticamente" << endl;
+			cout << "11 - Mostra estado fabrica" << endl;
+			cout << "12 - Reset das posicoes dos Robots" << endl;
+			cout << "0 - Sair" << endl;
+		}
 		cin >> op;
 		switch(op){
 		case 1:
@@ -102,81 +106,112 @@ void menu(){
 				break;
 			}
 		case 2:
-			cout << "Matriz:" << endl;;
-			cout << fab.getMatriz();
-			cout << endl;
-			cout << "------------------------------------------------------------------------------" << endl;
-			fflush(stdin);
-			cin.get();
-			system("cls");
+			if(fab.getCont() == 4){
+				cout << "Matriz:" << endl;;
+				cout << fab.getMatriz();
+				cout << endl;
+				cout << "------------------------------------------------------------------------------" << endl;
+				fflush(stdin);
+				cin.get();
+				system("cls");
+			}else
+				cout << "Opcao ainda nao disponivel devido a nao carregamento de todos os ficheiros!" << endl;
+			
 			break;
 		case 3:
-			cout << "Lista: "  << endl;
-			cout << "Robot: " << endl;
-			cout << fab.getLista();
-			cout << "------------------------------------------------------------------------------" << endl;
-			fflush(stdin);
-			cin.get();
-			system("cls");
+			if(fab.getCont() == 4){
+				cout << "Lista: "  << endl;
+				cout << "Robot: " << endl;
+				cout << fab.getLista();
+				cout << "------------------------------------------------------------------------------" << endl;
+				fflush(stdin);
+				cin.get();
+				system("cls");
+			}else
+				cout << "Opcao ainda nao disponivel devido a nao carregamento de todos os ficheiros!" << endl;
 			break;
 		case 4:
-			cout << "Queue: " << endl;
-			cout << fab.getQueue();
-			cout << "------------------------------------------------------------------------------" << endl;
-			fflush(stdin);
-			cin.get();
-			system("cls");
+			if(fab.getCont() == 4){
+				cout << "Queue: " << endl;
+				cout << fab.getQueue();
+				cout << "------------------------------------------------------------------------------" << endl;
+				fflush(stdin);
+				cin.get();
+				system("cls");
+			}else
+				cout << "Opcao ainda nao disponivel devido a nao carregamento de todos os ficheiros!" << endl;
 			break;
 		case 5:
-			cout << "Grafo: " << endl;
-			fab.getGrafo().escreve_grafo();
-			fflush(stdin);
-			cin.get();
-			system("cls");
+			if(fab.getCont() == 4){
+				cout << "Grafo: " << endl;
+				fab.getGrafo().escreve_grafo();
+				fflush(stdin);
+				cin.get();
+				system("cls");
+			}else
+				cout << "Opcao ainda nao disponivel devido a nao carregamento de todos os ficheiros!" << endl;
 			break;
 		case 6:
-			fab.criaGrafo();
-			fflush(stdin);
-			cin.get();
-			system("cls");
+			if(fab.getCont() == 4){
+				fab.criaGrafo();
+				fflush(stdin);
+				cin.get();
+				system("cls");
+			}else
+				cout << "Opcao ainda nao disponivel devido a nao carregamento de todos os ficheiros!" << endl;
 			break;
 		case 7:
-			fab.validaGrafo();
-			fflush(stdin);
-			cin.get();
-			system("cls");
+			if(fab.getCont() == 4){
+				fab.validaGrafo();
+				fflush(stdin);
+				cin.get();
+				system("cls");
+			}else
+				cout << "Opcao ainda nao disponivel devido a nao carregamento de todos os ficheiros!" << endl;
 			break;
 		case 8:
-			int key;
-			float qnt;
+			if(fab.getCont() == 4){
+				int key;
+				float qnt;
 
-			cout << "Insira o posto automatico que quer abastecer" << endl;
-			cin >> key;
-			cout << "Insira a quantidade de abastecimento" << endl;
-			cin >> qnt;
-			fab.abasteceAuto(key,qnt);
-			fflush(stdin);
-			cin.get();
-			system("cls");
+				cout << "Insira o posto automatico que quer abastecer" << endl;
+				cin >> key;
+				cout << "Insira a quantidade de abastecimento" << endl;
+				cin >> qnt;
+				fab.abasteceAuto(key,qnt);
+				fflush(stdin);
+				cin.get();
+				system("cls");
+			}else
+				cout << "Opcao ainda nao disponivel devido a nao carregamento de todos os ficheiros!" << endl;
 			break;
 		case 9:
+			if(fab.getCont() == 4){
+				int key;
+				cout << "Insira o posto armazem que quer abastecer" << endl;
+				cin >> key;
 
-			cout << "Insira o posto armazem que quer abastecer" << endl;
-			cin >> key;
-
-			fab.abasteceArm(key);
-			fflush(stdin);
-			cin.get();
-			system("cls");
+				fab.abasteceArm(key);
+				fflush(stdin);
+				cin.get();
+				system("cls");
+			}else
+				cout << "Opcao ainda nao disponivel devido a nao carregamento de todos os ficheiros!" << endl;
 			break;
 		case 10:
-			fab.abasteceAutomatico();
-			fflush(stdin);
-			cin.get();
-			system("cls");
+			if(fab.getCont() == 4){
+				fab.abasteceAutomatico();
+				fflush(stdin);
+				cin.get();
+				system("cls");
+			}else
+				cout << "Opcao ainda nao disponivel devido a nao carregamento de todos os ficheiros!" << endl;
 			break;
 		case 11:
-			fab.imprimeEstado();
+			if(fab.getCont() == 4)
+				fab.imprimeEstado();
+			else
+				cout << "Opcao ainda nao disponivel devido a nao carregamento de todos os ficheiros!" << endl;
 			break;
 
 		case 112: //TODO remover
